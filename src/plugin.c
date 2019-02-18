aea2f57e9192e0eba1e122af08f9a4a011e11d95
Dominik Schütz <do.sch.dev@gmail.com>
2019-02-18 11:03:53 +0100

    using correct copyright header now

--- src/plugin.c
+++ src/plugin.c
@@ -3,9 +3,10 @@
  *
  * Copyright (C) 2019 Dominik Schütz <do.sch.dev@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3, as published
- * by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranties of
@@ -13,7 +14,8 @@
  * PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * with this program;  if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #define _GNU_SOURCE
